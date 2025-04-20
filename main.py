b6fb1a877d8b4166fe0b51f0a4a1ddb102f49bf1
Hanif Jilani <hanif@umich.edu>
2025-04-20 14:09:52 -0400

    Created Crime Table and inserted 100 rows in get_crime_data()

--- main.py
+++ main.py
@@ -3,29 +3,31 @@ import sqlite3
 import os
 
 
-def get_crime_data(date):
+def get_crime_data(cur):
     crime_url = "https://data.cityofchicago.org/resource/ijzp-q8t2.json"
-    params = {
-        "$where": f"date between '{date}T00:00:00' and '{date}T23:59:59'",
-        "$select": "count(*)"
-    }
-    response = requests.get(crime_url, params=params)
-    if response.status_code == 200:
-        data = response.json()
-        if data:
-            return data[0]["count"]
+    cur.execute('SELECT date_time FROM Weather')
+    dates = cur.fetchall()
+    cur.execute("CREATE TABLE IF NOT EXISTS Crime (id INTEGER PRIMARY KEY AUTOINCREMENT, incident_reports INTEGER, date_time TEXT)")
+    for date in dates:
+        date = date[0]
+        params = {
+            "$where": f"date between '{date}T00:00:00' and '{date}T23:59:59'",
+            "$select": "count(*)"
+        }
+        response = requests.get(crime_url, params=params)
+        if response.status_code == 200:
+            data = response.json()
+            cur.execute("INSERT OR IGNORE INTO Crime (incident_reports, date_time) VALUES (?,?)", (data[0]['count'], date))  
         else:
-            return f"No data found for {date}."
-    else:
-        return f"Error: {response.status_code}, Message: {response.text}"
+            return f"Error: {response.status_code}, Message: {response.text}"
 
-def get_weather_data():
+def get_weather_data(cur):
     location = "Chicago"
     date_range = "2025-01-11/2025-04-20"
     weather_url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{location}/{date_range}?key=44SZFD9YD4PU6UJDGPBX273CX&include=days&elements=datetime,temp,conditions,precip,preciptype"
-    path = os.path.dirname(os.path.abspath(__file__))
-    conn = sqlite3.connect(path + "/" + "weather_and_crime.db")
-    cur = conn.cursor()
+    # path = os.path.dirname(os.path.abspath(__file__))
+    # conn = sqlite3.connect(path + "/" + "weather_and_crime.db")
+    # cur = conn.cursor()
     response = requests.get(weather_url)
     data = response.json()
     tbl = []
@@ -35,11 +37,15 @@ def get_weather_data():
     cur.execute("CREATE TABLE IF NOT EXISTS Weather (date_time TEXT PRIMARY KEY, temp INTEGER, conditions TEXT, precip INTEGER)")
     for days in tbl:
         cur.execute("INSERT OR IGNORE INTO Weather (date_time, temp, conditions, precip) VALUES (?, ?, ?, ?)", days)
-    conn.commit()
+    
 
 def main():
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path + "/" + "weather_and_crime.db")
+    cur = conn.cursor()
     # get_weather_data()
-    # print(f"Incident Reports: {get_crime_data('2024-04-04')}")
+    get_crime_data(cur)
+    conn.commit()
     pass
 
 if __name__ == "__main__":
