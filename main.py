e80e8b27612b809ce36517e2ea7cb7652ea486b0
Obeida Iqbal <obeidaiqbal41@gmail.com>
2025-04-20 14:25:40 -0400

    added an incremental id to the weather data when pushed to database

--- main.py
+++ main.py
@@ -25,16 +25,13 @@ def get_weather_data(cur):
     location = "Chicago"
     date_range = "2025-01-11/2025-04-20"
     weather_url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{location}/{date_range}?key=44SZFD9YD4PU6UJDGPBX273CX&include=days&elements=datetime,temp,conditions,precip,preciptype"
-    # path = os.path.dirname(os.path.abspath(__file__))
-    # conn = sqlite3.connect(path + "/" + "weather_and_crime.db")
-    # cur = conn.cursor()
     response = requests.get(weather_url)
     data = response.json()
     tbl = []
     for day in data['days']:
         lst = [day['datetime'], day['temp'], day['conditions'], day['precip']]
         tbl.append(lst)
-    cur.execute("CREATE TABLE IF NOT EXISTS Weather (date_time TEXT PRIMARY KEY, temp INTEGER, conditions TEXT, precip INTEGER)")
+    cur.execute("CREATE TABLE IF NOT EXISTS Weather (id INTEGER PRIMARY KEY AUTOINCREMENT, date_time TEXT, temp INTEGER, conditions TEXT, precip INTEGER)")
     for days in tbl:
         cur.execute("INSERT OR IGNORE INTO Weather (date_time, temp, conditions, precip) VALUES (?, ?, ?, ?)", days)
     
@@ -43,9 +40,10 @@ def main():
     path = os.path.dirname(os.path.abspath(__file__))
     conn = sqlite3.connect(path + "/" + "weather_and_crime.db")
     cur = conn.cursor()
-    # get_weather_data()
+    get_weather_data(cur)
     get_crime_data(cur)
     conn.commit()
+    conn.close()
     pass
 
 if __name__ == "__main__":
